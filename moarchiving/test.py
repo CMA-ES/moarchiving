ce1d1b28789a276185a2b112819142322007a784
nikohansen <nikolaus.hansen@inria.fr>
2020-05-13 23:16:23 +0200

    moved test call from main to test.py

--- moarchiving/test.py
+++ moarchiving/test.py
@@ -1,6 +1,5 @@
 import doctest
 import moarchiving
 
-print('deprecated: rather use ``python -m moarchiving.test``')
 print('doctest.testmod(moarchiving.moarchiving)')
 print(doctest.testmod(moarchiving.moarchiving))

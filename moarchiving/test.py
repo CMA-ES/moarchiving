3849c2218bfd586e1fc1fcfd4d11174cb35ea498
nikohansen <nikolaus.hansen@inria.fr>
2021-05-21 23:55:08 +0200

    test module also makes expensive asserts

--- moarchiving/test.py
+++ moarchiving/test.py
@@ -1,5 +1,12 @@
 import doctest
 import moarchiving
 
-print('doctest.testmod(moarchiving)')
-print(doctest.testmod(moarchiving))
+tmp = moarchiving.BiobjectiveNondominatedSortedList.make_expensive_asserts
+moarchiving.BiobjectiveNondominatedSortedList.make_expensive_asserts = True
+# print(moarchiving.moarchiving.BiobjectiveNondominatedSortedList.make_expensive_asserts)
+
+print('doctest.testmod(moarchiving.moarchiving)')
+print(doctest.testmod(moarchiving.moarchiving))
+
+moarchiving.BiobjectiveNondominatedSortedList.make_expensive_asserts = tmp
+# print(moarchiving.moarchiving.BiobjectiveNondominatedSortedList.make_expensive_asserts)

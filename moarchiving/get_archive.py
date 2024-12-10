3531a7b1ab74c88e168e1ef9c7755f80c89f0b6d
Nace Sever <47631585+naceee@users.noreply.github.com>
2024-12-10 14:22:13 +0100

    replaced asserts with warnings and errors

--- moarchiving/get_archive.py
+++ moarchiving/get_archive.py
@@ -18,10 +18,11 @@ def get_mo_archive(list_of_f_vals=None, reference_point=None, infos=None, n_obj=
     Factory function for creating MOArchive objects of the appropriate dimensionality.
 
     Args:
-        list_of_f_vals: list of objective vectors, can be None if n_obj is provided
+        list_of_f_vals: list of objective vectors
         reference_point: reference point for the archive
         infos: list of additional information for each objective vector
-        n_obj: must be provided if list_of_f_vals is None
+        n_obj: when initializing an empty archive, the number of objectives should be provided
+            to determine the dimensionality of the archive (default is 2)
 
     Returns:
         MOArchive object of the appropriate dimensionality, based on the number of objectives
@@ -37,8 +38,8 @@ def get_mo_archive(list_of_f_vals=None, reference_point=None, infos=None, n_obj=
         except:
             get_mo_archive.hypervolume_computation_float_type = float
 
-    assert list_of_f_vals is not None or n_obj is not None or reference_point is not None, \
-        "At least one of `list_of_f_vals`, `reference_point` or `n_obj` must be provided"
+    if (list_of_f_vals is None or len(list_of_f_vals) == 0) and n_obj is None and reference_point is None:
+        n_obj = 2
 
     if n_obj is None:
         if list_of_f_vals is not None and len(list_of_f_vals) > 0:
@@ -46,12 +47,27 @@ def get_mo_archive(list_of_f_vals=None, reference_point=None, infos=None, n_obj=
         else:
             n_obj = len(reference_point)
 
-    if list_of_f_vals is not None and len(list_of_f_vals) > 0:
-        assert len(list_of_f_vals[0]) == n_obj, \
-            "The number of objectives in list_of_f_vals must match n_obj"
-    if reference_point is not None:
-        assert len(reference_point) == n_obj, \
-            "The number of objectives in reference_point must match n_obj"
+    # check if the number of objectives matches the number of objectives in the list of f_vals
+    # and the reference point if they are provided and not empty
+    if list_of_f_vals is not None and len(list_of_f_vals) > 0 and reference_point is not None:
+        if len(reference_point) != len(list_of_f_vals[0]):
+            raise ValueError(f"n_obj ({len(reference_point)}) does not match the number of "
+                             f"objectives in the first element of list_of_f_vals "
+                             f"({len(list_of_f_vals[0])})")
+        elif n_obj != len(list_of_f_vals[0]):
+            _warnings.warn(f"n_obj ({n_obj}) does not match the number of objectives in "
+                           f"list_of_f_vals ({len(list_of_f_vals[0])})")
+            n_obj = len(list_of_f_vals[0])
+    elif list_of_f_vals is not None and len(list_of_f_vals) > 0:
+        if n_obj != len(list_of_f_vals[0]):
+            _warnings.warn(f"n_obj ({n_obj}) does not match the number of objectives in "
+                           f"list_of_f_vals ({len(list_of_f_vals[0])})")
+            n_obj = len(list_of_f_vals[0])
+    elif reference_point is not None:
+        if n_obj != len(reference_point):
+            _warnings.warn(f"n_obj ({n_obj}) does not match the number of objectives in "
+                           f"reference_point ({len(reference_point)})")
+            n_obj = len(reference_point)
 
     if n_obj == 2:
         return MOArchive2d(list_of_f_vals, reference_point=reference_point, infos=infos,
@@ -75,11 +91,12 @@ def get_cmo_archive(list_of_f_vals=None, list_of_g_vals=None, reference_point=No
     Function for creating CMOArchive objects, with similar interface as get_mo_archive.
 
     Args:
-        list_of_f_vals: list of objective vectors, can be None if n_obj is provided
+        list_of_f_vals: list of objective vectors
         list_of_g_vals: list of constraint vectors, must be the same length as list_of_f_vals
         reference_point: reference point for the archive
         infos: list of additional information for each objective vector
-        n_obj: must be provided if list_of_f_vals is None
+        n_obj: should be provided when initializing an empty archive,
+            to determine the dimensionality of the archive (default is 2)
         tau: threshold that indicates when the indicator reaches feasibility
     Returns:
         MOArchive object of the appropriate dimensionality, based on the number of objectives
@@ -95,6 +112,44 @@ def get_cmo_archive(list_of_f_vals=None, list_of_g_vals=None, reference_point=No
             get_cmo_archive.hypervolume_computation_float_type = fractions.Fraction
         except:
             get_cmo_archive.hypervolume_computation_float_type = float
+
+    if (list_of_f_vals is None or len(list_of_f_vals) == 0) and n_obj is None and reference_point is None:
+        n_obj = 2
+
+    if n_obj is None:
+        if list_of_f_vals is not None and len(list_of_f_vals) > 0:
+            n_obj = len(list_of_f_vals[0])
+        else:
+            n_obj = len(reference_point)
+
+    # check if the number of objectives matches the number of objectives in the list of f_vals
+    # and the reference point if they are provided and not empty
+    if list_of_f_vals is not None and len(list_of_f_vals) > 0 and reference_point is not None:
+        if len(reference_point) != len(list_of_f_vals[0]):
+            raise ValueError(f"n_obj ({len(reference_point)}) does not match the number of "
+                             f"objectives in the first element of list_of_f_vals "
+                             f"({len(list_of_f_vals[0])})")
+        elif n_obj != len(list_of_f_vals[0]):
+            _warnings.warn(f"n_obj ({n_obj}) does not match the number of objectives in "
+                           f"list_of_f_vals ({len(list_of_f_vals[0])})")
+            n_obj = len(list_of_f_vals[0])
+    elif list_of_f_vals is not None and len(list_of_f_vals) > 0:
+        if n_obj != len(list_of_f_vals[0]):
+            _warnings.warn(f"n_obj ({n_obj}) does not match the number of objectives in "
+                           f"list_of_f_vals ({len(list_of_f_vals[0])})")
+            n_obj = len(list_of_f_vals[0])
+    elif reference_point is not None:
+        if n_obj != len(reference_point):
+            _warnings.warn(f"n_obj ({n_obj}) does not match the number of objectives in "
+                           f"reference_point ({len(reference_point)})")
+            n_obj = len(reference_point)
+
+    if list_of_f_vals is None and list_of_g_vals is not None:
+        raise ValueError("list_of_f_vals must be provided if list_of_g_vals is provided")
+    if list_of_f_vals is not None and list_of_g_vals is None:
+        raise ValueError("list_of_g_vals must be provided if list_of_f_vals is provided")
+    if list_of_f_vals is not None and list_of_g_vals is not None and len(list_of_f_vals) != len(list_of_g_vals):
+        raise ValueError("list_of_f_vals and list_of_g_vals must have the same length")
 
     return CMOArchive(list_of_f_vals=list_of_f_vals, list_of_g_vals=list_of_g_vals,
                       reference_point=reference_point, infos=infos, n_obj=n_obj, tau=tau,
